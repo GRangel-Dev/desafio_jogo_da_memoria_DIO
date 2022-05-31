b11e837ec4e68d63932b443a16c7c19b4ab27c7d
GRangel-Dev <rantro.dev@gmail.com>
2022-05-31 19:08:50 -0300

    primeira fase da construção do jogo

---
+++ assets/JS/script.js
@@ -0,0 +1,34 @@
+const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+
+function flipcard(){
+     this.classList.add('flip');
+
+     if(!hasFlippedCard){
+          hasFlippedCard = true;
+          firstCard = this;
+          return;
+     }
+
+     secondCard = this;
+     hasFlippedCard = false;
+     checkForMath();
+}
+
+function checkForMath(){
+     if(firstCard.dataset.card === secondCard.dataset.card){
+      disableCards();
+      return;    
+     }
+
+     unflipCards();
+}
+
+cards.forEach((card) => {
+     card.addEventListener('click', flipcard)
+})
+
+
+
