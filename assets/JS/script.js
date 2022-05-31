5c3dcd2adc407c3c652210346eca7f7cdbd6dbf2
GRangel-Dev <rantro.dev@gmail.com>
2022-05-31 20:02:17 -0300

    finalizado o projeto

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,9 +1,13 @@
 const cards = document.querySelectorAll('.card');
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let blockBoard = false;
 
 
 function flipcard(){
+     if(blockBoard) return;
+     if(this === firstCard) return;
+
      this.classList.add('flip');
 
      if(!hasFlippedCard){
@@ -26,9 +30,41 @@ function checkForMath(){
      unflipCards();
 }
 
+function disableCards(){
+     firstCard.removeEventListener('click', flipcard);
+     secondCard.removeEventListener('click', flipcard);
+     
+     resetBoard();
+}
+
+function unflipCards(){
+     blockBoard = true;
+
+     setTimeout(()=>{
+          firstCard.classList.remove('flip');
+          secondCard.classList.remove('flip');
+
+          resetBoard();
+     }, 1500);
+}
+
+function resetBoard(){
+     [hasFlippedCard. blockBoard] = [false, false];
+     [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle(){
+     cards.forEach((card) => {
+          let ramdomPosition = Math.floor(Math.random() * 12);
+          card.style.order = ramdomPosition;
+     })
+})();
+
 cards.forEach((card) => {
      card.addEventListener('click', flipcard)
-})
+});
+
+
 
 
 
